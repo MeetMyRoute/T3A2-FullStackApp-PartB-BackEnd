f7f72007a22ee53b9a912cc3f006cf05b9eec163
kimnle <kimnle002@gmail.com>
2024-11-27 17:18:36 +1100

    initiliased setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const {app} = require("./server.js");
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
