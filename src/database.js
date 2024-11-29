a216924cc56b6cadd984df42d547cd46bb07d658
kimnle <kimnle002@gmail.com>
2024-11-29 15:58:05 +1100

    created db connection function

--- src/database.js
+++ src/database.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
 async function dbConnect() {
-    let dbUrl = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/${}`;
+    let dbUrl = process.env.DB_URI || "mongodb://localhost:27017/MeetMyRoute";
+    await mongoose.connect(dbUrl);
+    console.log("Database connected")
+}
+
+module.exports = {
+    dbConnect
 }
