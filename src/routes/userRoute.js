a71cdb4de41231f9f1eb58a637b43d950b7645a6
sooffia <sofia23kabir@gmail.com>
2024-12-06 18:42:39 +1100

    add admin route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { auth } = require("../middleware/auth"); 
+const { auth, adminAuth } = require('../middleware/auth');
 
 const {
   registerUser,
   loginUser,
   recieveLoggedInUser, 
+  adminLogin, 
 } = require("../controllers/userController");
 
 // Route to register a new user
 router.post("/", registerUser);
 // Route to log in an existing user and generate a token
 router.post("/login", loginUser);
-// Route to log in an existing user and generate a token
-router.get("/me", auth, recieveLoggedInUser); 
+// Route to log in admin user and generate a token 
+router.post("/admin", auth, adminAuth, adminLogin)
+// Route to fetch the currently logged-in user's information
+router.get("/me", auth, recieveLoggedInUser);
 
 module.exports = router;
