fb5e002e55f09b6cd696dc4ef34dbc2ac78c215b
theresaxn <hongoanh3@hotmail.com>
2024-12-08 22:30:28 +1100

    updated getProfile and updateProfile controller functions to include profilePic field

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -10,17 +10,43 @@ const getProfile = async (request, response) => {
         // Find user profile that belong to the user ID
         const user = await UserModel.findById(id);
 
-        // Check if user ID exists
+        // Check if the user ID exists
+        // If it does not, return error
         if (!user) {
             return response.status(404).json({
                 message: "User not found"
             })
         }
 
+        // Check if status is set to Private
+        // If it is, return error
+        if (user.status == "Private") {
+            return response.status(403).json({
+                message: "User profile is private"
+            })
+        }
+
+        // Check if a profile pic exists
+        // If it does, convert it to Base64
+        let profilePicUrl = null;
+        if (user.profilePic) {
+            // Convert the Buffer binary data to a Base64 string
+            const base64Image = user.profilePic.toString("base64");
+            // Create a data URL format
+            profilePicUrl = `data:${user.profilePic.contentType};base64,${base64Image}`;
+        }
+
         // Respond with the found user profile
         response.status(200).json({
             message: "User profile retrieved successfully",
-            data: user
+            data: {
+                name: user.name,
+                location: user.location, 
+                status: user.status,
+                profilePic: profilePicUrl,
+                travelPreferencesAndGoals: user.travelPreferencesAndGoals,
+                socialMediaLink: user.socialMediaLink
+            }
         });
     } catch (error) {
         response.status(500).json({
@@ -36,7 +62,7 @@ const updateProfile = async (request, response) => {
         // Get the user ID from the URL parameters
         const {id} = request.params;
         // Get data to update from the request body
-        const {name, location, status, travelPreferencesAndGoals, socialMediaLink} = request.body;
+        const {name, location, status, profilePic, travelPreferencesAndGoals, socialMediaLink} = request.body;
 
         // Get user ID from JWT token
         const loggedInUserId = request.user.id;
@@ -64,6 +90,24 @@ const updateProfile = async (request, response) => {
         user.status = status || user.status;
         user.travelPreferencesAndGoals = travelPreferencesAndGoals || user.travelPreferencesAndGoals;
         user.socialMediaLink = socialMediaLink || user.socialMediaLink;
+
+        // If new data is provided for profilePic, convert it into a Buffer object
+        if (profilePic) {
+            // Extract MIME type
+            const matches = profilePic.match(/^data:(.+);base64,/);
+            if (matches && matches[1]) {
+                const mimeType = matches[1];
+                const base64Data = profilePic.replace(/^data:.+;base64,/, '');
+                const buffer = Buffer.from(base64Data, "base64");
+
+                // Update field
+                user.profilePic = {data: buffer, contentType: mimeType}
+            } else {
+                return response.status(400).json({
+                    message: "Invalid image format"
+                })
+            }
+        }
 
         // Save updated user profile
         const updatedUser = await user.save();
