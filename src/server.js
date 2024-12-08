20fc1e2fbeba0895e0c81ec95a4030914977d1ab
sooffia <sofia23kabir@gmail.com>
2024-12-09 02:07:08 +1100

    fetch server.js from main branch and add user route

--- src/server.js
+++ src/server.js
@@ -3,15 +3,20 @@ const cors = require("cors");
 
 const app = express();
 
+// Update when deployed
 app.use(cors());
 app.use(express.json());
 
-// Testing basic route
+// Test basic route
 app.get("/", (request, response) => {
     response.json({
         message: "MeetMyRoute"
     });
 });
+
+const ProfileRouter = require("./routes/ProfileRoute");
+app.use("profile", ProfileRouter);
+app.use('/api/users', require('./routes/userRoute')); 
 
 module.exports = {
     app
