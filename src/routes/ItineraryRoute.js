562186e07765a59a5acb82f4f503031b749ccb20
kimnle <kimnle002@gmail.com>
2024-12-09 18:24:05 +1100

    fixed itinerary route names and removed a controller function

--- src/routes/ItineraryRoute.js
+++ src/routes/ItineraryRoute.js
@@ -1,30 +1,27 @@
 const express = require("express");
-const { createItinerary, getItineraries, getSimplifiedItineraries, getSharedItineraries, getSharedItinerariesByUser, deleteItinerary, updateItinerary, getSharedItinerariesByFilters } = require("../controllers/ItineraryController");
+const { createItinerary, getItineraries, getSimplifiedItineraries, getSharedItinerariesByUser, deleteItinerary, updateItinerary, getSharedItinerariesByFilters } = require("../controllers/ItineraryController");
 const router = express.Router();
 // const auth = require();
 
 // Route for creating a new itinerary to the database
-router.post("/itinerary", createItinerary);
+router.post("/", createItinerary);
 
 // Route for getting all itineraries owned by the user
-router.get("/itineraries", getItineraries);
+router.get("/", getItineraries);
 
 // Route for getting all simplified itineraries owned by the user
-router.get("/itineraries/simplified", getSimplifiedItineraries);
-
-// Route for getting all shared (simplified) itineraries excluding the user's
-router.get("/itineraries/shared", getSharedItineraries);
+router.get("/simplified", getSimplifiedItineraries);
 
 // Route for getting shared (simplified) itineraries excluding the user's by filters: destination, startDate and endDate
-router.get("/itineraries/shared/filtered", getSharedItinerariesByFilters);
+router.get("/search/filtered", getSharedItinerariesByFilters);
 
 // Route for getting shared (simplified) itineraries by a specific user
-router.get("/itineraries/user/:userId", getSharedItinerariesByUser);
+router.get("/user/:userId", getSharedItinerariesByUser);
 
 // Route for updating details of a specific itinerary
-router.put("/itineraries/:id", updateItinerary);
+router.put("/:id", updateItinerary);
 
 // Route for deleting an itinerary from the database
-router.delete("/itineraries/:id", deleteItinerary);
+router.delete("/:id", deleteItinerary);
 
-module.exports = router
+module.exports = router;
