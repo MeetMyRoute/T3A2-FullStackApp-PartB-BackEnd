4a18ac13a366d10473e311b2bc8ed16b3d664ef0
sooffia <sofia23kabir@gmail.com>
2024-12-10 16:36:16 +1100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
+const { dbConnect } = require("./utils/database.js");
 const {app} = require("./server.js");
 
+dbConnect();
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
