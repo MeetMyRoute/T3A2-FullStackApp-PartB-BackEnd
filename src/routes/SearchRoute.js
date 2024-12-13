84682cb143591dddf553635b24389578c30228d1
kimnle <kimnle002@gmail.com>
2024-12-13 21:50:50 +1100

    moved get itineraries and users by filter route to search route file

---
+++ src/routes/SearchRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { getItinerariesAndUsersByFilters } = require("../controllers/ItineraryController");
+const { auth } = require("../middleware/auth");
+const router = express.Router();
+
+// Route for getting shared (simplified) itineraries and local users excluding the user's by filters: destination, startDate and endDate
+router.get("/", auth, getItinerariesAndUsersByFilters);
