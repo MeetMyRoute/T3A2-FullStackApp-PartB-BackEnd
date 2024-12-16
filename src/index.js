89d049a876877786d9de773cc3ed613e113c370d
kimnle <kimnle002@gmail.com>
2024-12-16 16:38:46 +1100

    setup web socket server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const { dbConnect } = require("./utils/database.js");
-const {app} = require("./server.js");
+const { app, server } = require("./server.js");
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, async() => {
+server.listen(PORT, async() => {
     await dbConnect();
     console.log(`Server is running on port ${PORT}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+// Required for creating a HTTP server
+const http = require("http");
+const {Server} = require("socket.io");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
-// Update when deployed
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +17,7 @@ app.get("/", (req, res) => {
     });
 });
 
+// Routes
 const ProfileRouter = require("./routes/ProfileRoute");
 app.use("/profile", ProfileRouter);
 
@@ -26,6 +30,41 @@ app.use('/user', UserRouter);
 const SearchRouter = require("./routes/SearchRoute");
 app.use("/search", SearchRouter);
 
+// Create HTTP server and attach Socket.IO
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+
+        // Allow all origins
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+    }
+});
+
+// Socket.IO logic
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    // Listen for incoming messages
+    socket.on("sendMessage", (messageData) => {
+        console.log("Messages received:", messageData);
+
+        // Broadcast the message to all clients in a room
+        io.to(messageData.room).emit("receiveMessage", messageData);
+    });
+
+    // Listen for joining a chat room
+    socket.on("joinRoom", (room) => {
+        socket.join(room);
+        console.log(`User ${socket.id} joined room ${room}`);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
 module.exports = {
-    app
+    app,
+    server
 }
