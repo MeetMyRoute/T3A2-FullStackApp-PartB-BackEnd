40923e841bf0aa7e52688a1e37d0082efca556b7
sooffia <sofia23kabir@gmail.com>
2024-12-17 16:38:36 +1100

    add upload middleware to UserRoute

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { auth } = require('../middleware/auth');
+const { auth } = require('../middleware/auth'); 
+const upload = require('../middleware/uploadConfig');
 
 const {
-  getAllUsers, 
   registerUser,
   loginUser,
-  getUserById,
+  getAllUsers, 
   recieveLoggedInUser,  
   adminLogin, 
   forgetPassword, 
@@ -14,15 +14,15 @@ const {
 } = require("../controllers/UserController");
 
 // Route to register a new user
-router.post("/", registerUser);
+// router.post("/", registerUser);
+// Route to register a new user with profile picture upload
+router.post("/", upload.single("profilePicture"), registerUser);
 // Route to log in an existing user and generate a token
 router.post("/login", loginUser);
 // Route to log in admin user and generate a token 
 router.post("/adminLogin",adminLogin);
 // Route to get all users (requires admin authentication)
 router.get("/", auth, getAllUsers);
-// Route to get user by ID
-router.get("/:id", getUserById)
 // Route to fetch the currently logged-in user's information
 router.get("/me", auth, recieveLoggedInUser);
 // Route to handle forgotten password requests
